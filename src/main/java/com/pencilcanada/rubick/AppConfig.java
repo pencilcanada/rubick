a2e2c547cb0348ded663dc47545a65f2d8411c78
Runzhuo Li <runzhuo1990@gmail.com>
2017-12-08 11:24:23 -0500

    Update MongoDB config to use SSL for reactive stream.

---
+++ src/main/java/com/pencilcanada/rubick/AppConfig.java
@@ -0,0 +1,28 @@
+package com.pencilcanada.rubick;
+
+import com.mongodb.reactivestreams.client.MongoClient;
+import com.mongodb.reactivestreams.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+
+@Configuration
+@EnableReactiveMongoRepositories
+public class AppConfig extends AbstractReactiveMongoConfiguration {
+
+    @Value("${spring.data.mongodb.uri}")
+    String mongoUri;
+
+    @Override
+    protected String getDatabaseName() {
+        return "rubick";
+    }
+
+    @Override
+    @Bean
+    public MongoClient reactiveMongoClient() {
+        return MongoClients.create(mongoUri);
+    }
+}
